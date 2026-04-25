591fdd79d7cfbcf23117e0b5caa44f0eafbc7df1
camilita711 <camilita711@gmail.com>
2026-04-24 23:55:47 -0400

    Feat:modificate first test

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -1,11 +1,15 @@
+import { registerPage, RegisterPage } from "../support/Pages/signUpPages.js"
+
 beforeEach( () => {
     cy.visit('/login')
 })
 
-describe('Realizar el login de la pag Automation',() => {
-    it('Hacer el login con las credenciales correctas', () => {
-    cy.get('[data-qa="login-email"]').type('pepapig@gmail.com')
-    cy.get('[data-qa="login-password"]').type('Tupepapig.com')
-    cy.get('[data-qa="login-button"]').click()
-})
+describe('test',() => {
+    it('first test', () =>{
+        cy.fixture('user').then(user =>{
+            registerPage.fillFormInit(user)
+        })
+        
+
+    })
 })
