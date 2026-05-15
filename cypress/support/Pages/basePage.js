4a8671f994034a760aa198883b3c54ec2cc15eb1
camilita711 <camilita711@gmail.com>
2026-05-15 14:24:20 -0400

    fix: add new method (mouseover)

--- cypress/support/Pages/basePage.js
+++ cypress/support/Pages/basePage.js
@@ -2,18 +2,26 @@ export class BasePage {
     type(selector,data){
         cy.get(selector).type(data)
     }
+
+    mouseover(selector){
+        cy.get(selector).trigger('mouseover', {force: true})
+    }
+
     click(selector){
         cy.get(selector).click({force: true})
     }
+
     select(selector,value){
         cy.get(selector).select(value)
     }
+
     mouseoverIndex(selector, index = 0){
         cy.get(selector).eq(index).trigger('mouseover', {force: true})
     }
+
     clickIndex(selector, index = 0) {
         cy.get(selector).eq(index).click({force: true})
-}
+    }
 }
 
 export const basePage = new BasePage()
