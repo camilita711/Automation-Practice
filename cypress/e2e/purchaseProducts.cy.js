d046ad5d969c720cb164d7c9d77af088845d0236
camilita711 <camilita711@gmail.com>
2026-05-27 10:29:47 -0400

    refactor: remove duplicate file and rename core module

--- cypress/e2e/purchaseProducts.cy.js
+++ cypress/e2e/purchaseProducts.cy.js
@@ -1,98 +1,93 @@
-import { productsPage, ProductsPage } from '../support/Pages/productsPage.js';
-import { ProductsAssertions } from '../support/Assertions/productAssertions.js';
+import { productsPage } from '../support/Pages/productsPage.js';
 import { cartPage } from '../support/Pages/cartPage.js';
-import { cartAssertions, CartAssertions } from '../support/Assertions/cartAssertions.js';
-import { checkoutPage, CheckoutPage } from '../support/Pages/checkoutPage.js';
+import { checkoutPage } from '../support/Pages/checkoutPage.js';
+
+import { cartAssertions } from '../support/Assertions/cartAssertions.js';
 import { checkoutAssertions } from '../support/Assertions/checkoutAssertions.js';
+import { paymentAssertions } from '../support/Assertions/paymentAssertions.js';
+
 import { paymentFlow } from '../support/Flows/paymentFlow.js';
-import { paymentAssertions, PaymentAssertions } from '../support/Assertions/paymentAssertions.js';
-// nota debo crear precondiciones que el carrito este vacio, con un condicional que cuando este vacio eliminar los productos del carrito
-// tambien debo crear las assertions de error para que no pasen los Test Cases mergear y hacer mi ci cd
-// ver mejores practicas para mejorar mi codigo
-describe('Purchase FLow', () => {
-  beforeEach(() => {
-    cy.fixture('user').then((user) => {
+
+describe('Purchase Flow', () => {
+  function goToPaymentPage(nProducts, qProducts) {
+    productsPage.addProduct(nProducts);
+    cartAssertions.validateCartUrl();
+    cartAssertions.validateProductQuantity(qProducts);
+    cartPage.procedToCheckout();
+    checkoutAssertions.validateCheckoutUrl();
+    checkoutPage.placeOrder();
+  }
+
+  beforeEach(function () {
+    cy.fixture('user').as('userData');
+
+    cy.fixture('cards').as('cardData');
+
+    cy.get('@userData').then((user) => {
       cy.login(user.validUser);
     });
+
     cy.visit('/products');
   });
+
   describe('Happy Path', () => {
-    it('Should  complete the purchase successfully', () => {
-      cy.fixture('user').then((user) => {
-        const validUser = user.validUser;
+    it('Should complete the purchase successfully', function () {
+      const validUser = this.userData.validUser;
 
-        cy.fixture('cards').then((cards) => {
-          const validCard = cards.validCard;
+      const validCard = this.cardData.validCard;
 
-          productsPage.addProduct(1);
-          cartAssertions.validateCartUrl();
-          cartAssertions.validateProductQuantity('1');
-          cartPage.procedToCheckout();
-          checkoutAssertions.validateInvoiceInformation(
-            `${validUser.firstName} ${validUser.lastName}`,
-            validUser.mobileNumber
-          );
-          checkoutPage.placeOrder();
-          paymentFlow(validCard);
-          paymentAssertions.validateOrderPlaced();
-        });
-      });
+      productsPage.addProduct(1);
+
+      cartAssertions.validateCartUrl();
+
+      cartAssertions.validateProductQuantity('1'); // same value that productsPage.addProduct(1)
+
+      cartPage.procedToCheckout();
+
+      checkoutAssertions.validateInvoiceInformation(
+        `${validUser.firstName} ${validUser.lastName}`,
+        validUser.mobileNumber
+      );
+
+      checkoutPage.placeOrder();
+
+      paymentFlow(validCard);
+
+      paymentAssertions.validateOrderPlaced();
     });
   });
 
   describe('Payment Validations', () => {
-    it('Should reject invalid card number', () => {
-      cy.fixture('cards').then((cards) => {
-        const invalidCard = cards.invalidCard;
+    it('Should reject invalid card', function () {
+      goToPaymentPage(1, 1);
 
-        productsPage.addProduct(1);
-        cartAssertions.validateCartUrl();
-        cartAssertions.validateProductQuantity('1');
-        cartPage.procedToCheckout();
-        checkoutAssertions.validateInvoiceInformation(
-          `${validUser.firstName} ${validUser.lastName}`,
-          validUser.mobileNumber
-        );
-        checkoutPage.placeOrder();
-        paymentFlow(invalidCard);
-        //paymentAssertions.validateInvalidCard()
-      });
+      paymentFlow(this.cardData.invalidCard);
+
+      paymentAssertions.validateInvalidCard();
     });
 
-    it('Should reject expired card', () => {
-      cy.fixture('cards').then((cards) => {
-        const expiredCard = cards.expiredCard;
+    it('Should reject expired card', function () {
+      goToPaymentPage(1, 1);
 
-        productsPage.addProduct(1);
-        cartPage.procedToCheckout();
-        checkoutPage.placeOrder();
-        paymentFlow(expiredCard);
-        // paymentAssertions.validateExpiredCard()
-      });
+      paymentFlow(this.cardData.expiredCard);
+
+      paymentAssertions.validateInvalidCard();
     });
 
-    it('Should validate required payment fields', () => {
-      cy.fixture('cards').then((cards) => {
-        const emptyCard = cards.emptyCard;
+    it('Should validate required payment fields', function () {
+      goToPaymentPage(1, 1);
 
-        productsPage.addProduct(1);
-        cartPage.procedToCheckout();
-        checkoutPage.placeOrder();
-        paymentFlow(emptyCard);
-        //paymentAssertions.validateRequiredFields()
-      });
+      paymentFlow(this.cardData.emptyCard);
+
+      paymentAssertions.validateInvalidCard();
     });
 
-    it('Should reject invalid cvc', () => {
-      cy.fixture('cards').then((cards) => {
-        const invalidCvc = cards.invalidCardCvc;
+    it('Should reject invalid cvc', function () {
+      goToPaymentPage(1, 1);
 
-        productsPage.addProduct(1);
-        cartPage.procedToCheckout();
-        checkoutPage.placeOrder();
-        paymentFlow(invalidCvc);
-        //paymentAssertions.validateInvalidCvc()
-      });
+      paymentFlow(this.cardData.invalidCardCvc);
+
+      paymentAssertions.validateInvalidCard();
     });
   });
 });
